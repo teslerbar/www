5baa0de274195d7dcdbd1bc51ef2bfa4ad92f06c
teslerbar <bar@themis-tech.io>
2023-04-21 15:53:45 +0300

    rying to parse the file

--- app/page.tsx
+++ app/page.tsx
@@ -11,29 +11,13 @@ export default async function Home() {
     filePath,
   });
 
+  console.log("what:", response);
+
   return (
     <>
       <p className="z-10 w-full max-w-xl px-5 xl:px-0">{response.toString()}</p>
       <div className="z-10 w-full max-w-xl px-5 xl:px-0">
-        <div className="grid grid-cols-4 gap-4">
-          <blockquote class="twitter-tweet">
-            <p lang="en" dir="ltr">
-              Working on a new playground to compare AI providers. I&#39;ll be
-              sharing some thoughts here as I go. My plan is to launch next
-              week.{" "}
-              <a href="https://t.co/v5g8Dv5Npj">pic.twitter.com/v5g8Dv5Npj</a>
-            </p>
-            &mdash; Jared Palmer (@jaredpalmer){" "}
-            <a href="https://twitter.com/jaredpalmer/status/1643627344475594752?ref_src=twsrc%5Etfw">
-              April 5, 2023
-            </a>
-          </blockquote>{" "}
-          <script
-            async
-            src="https://platform.twitter.com/widgets.js"
-            charset="utf-8"
-          ></script>
-        </div>
+        <div className="grid grid-cols-4 gap-4"></div>
       </div>
     </>
   );
--- utils/getGitHubFileContent.ts
+++ utils/getGitHubFileContent.ts
@@ -12,7 +12,7 @@ export async function getGitHubFileContent({ repoUrl, filePath, branchName}: Get
         headers: {
           "Authorization": `Bearer ${process.env.GITHUB_OAUTH_TOKEN}`,
           "Content-Type": "application/json",
-          Accept: 'application/vnd.github.v3.raw',
+          // "Accept": "application/vnd.github+json",
         },
       }),
       // data will revalidate every 60 seconds
@@ -26,6 +26,20 @@ export async function getGitHubFileContent({ repoUrl, filePath, branchName}: Get
     );
   
   
-    const content = await response.text();
+    const content = await response.json();
+      
+    if (content.encoding === "base64") {
+      const decodedContent = atob(content.content);
+
+      console.log("CONTENTTT: ",typeofdecodedContent)
+
+      
+      return JSON.parse(decodedContent);
+    } else {
+      throw new Error(`Unsupported encoding: ${content.encoding}`);
+    }
+    
+    // const trimmedContent = content.trim();
+
     return content;
   }
