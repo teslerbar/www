3e61993596a45354cb36ec9c88d92d3dfd17c0e9
teslerbar <bar@themis-tech.io>
2023-04-22 13:28:46 +0300

    I really need to install git to see diffs

--- components/home/TweetsFeed/TweetsFeed.tsx
+++ components/home/TweetsFeed/TweetsFeed.tsx
@@ -1,9 +1,18 @@
-"use client";
-
 import { Tweet } from "@/components/shared/Tweet/Tweet";
+import { getGitHubFileContent } from "@/utils/getGitHubFileContent";
 import { TweetsFeedProps } from "./TweetsFeed.types";
 
-export function TweetsFeed({ tweets }: TweetsFeedProps) {
+const repoUrl = "https://api.github.com/repos/teslerbar/www";
+const filePath = "content.json";
+const branchName = "main";
+
+export async function TweetsFeed() {
+  const { links: tweets } = await getGitHubFileContent({
+    repoUrl,
+    branchName,
+    filePath,
+  });
+
   return (
     <>
       {tweets.map(({ url }) => {
